056daa91c767f0e2c405c3c217c9e76c3796a976
Andrey Araujo <andreyaraujo.dev@gmail.com>
2024-02-07 16:39:14 -0300

    feat: listagem de carrinhos apenas para o email do usuario logado

--- src/app/(dasboard)/home/page.tsx
+++ src/app/(dasboard)/home/page.tsx
@@ -1,26 +1,28 @@
 'use client'
 
 import {
-  getFavoriteCarts,
-  useCarts
+  useCarts,
+  useFavoritesCarts
 } from '@/services/react-query/hooks/useCarts'
 import { CartCard } from './components/CartCard'
 import { Skeleton } from '@/components/ui/skeleton'
-import { useQuery } from 'react-query'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
-  const { data, isLoading } = useCarts()
-  const { data: favorites, isLoading: isLoadingFavorites } = useQuery(
-    ['favoriteCarts'],
-    getFavoriteCarts
+  const { data: session } = useSession()
+  const { data, isLoading } = useCarts(
+    session?.user?.email as string | undefined
   )
-
+  const { data: favorites, isLoading: isLoadingFavorites } = useFavoritesCarts(
+    session?.user?.email as string | undefined
+  )
   const imageURL =
     'https://i.pinimg.com/originals/ac/34/84/ac348422c1fd4d46f9a652f32839f8d6.jpg'
+
   return (
     <div className="flex w-full h-full p-6 flex-col space-y-5">
       <div className="w-full">
-        <h2 className="font-bold text-2xl mb-3">Favoritos</h2>
+        <h2 className="font-bold text-3xl mb-3">Favoritos</h2>
         <div className="flex items-center space-x-3 w-full flex-wrap">
           {isLoadingFavorites ? (
             <Skeleton className="w-96 m-2" />
@@ -49,11 +51,11 @@ export default function Home() {
       </div>
 
       <div className="w-full">
-        <h2 className="font-bold text-2xl mb-3">Coleção</h2>
+        <h2 className="font-bold text-3xl mb-3">Coleção</h2>
         <div className="flex items-center w-full flex-wrap">
           {isLoading ? (
             <Skeleton className="w-96 m-2" />
-          ) : (
+          ) : favorites && favorites?.length > 0 ? (
             data?.map((cart) => (
               <CartCard
                 key={cart._id}
@@ -71,6 +73,8 @@ export default function Home() {
                 isFavorite={cart.isFavorite}
               />
             ))
+          ) : (
+            <p>Você não adicionou nenhum carro à sua coleção.</p>
           )}
         </div>
       </div>
--- src/services/react-query/hooks/useCarts.ts
+++ src/services/react-query/hooks/useCarts.ts
@@ -5,18 +5,33 @@ import { UseQueryResult, useQuery } from 'react-query'
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 const api = createAxiosInstance(apiUrl as string)
 
-async function getCarts(): Promise<Cart[]> {
-  const { data } = await api.get<Cart[]>('/cart')
+async function getCarts(userEmail?: string): Promise<Cart[]> {
+  const { data } = await api.get<Cart[]>(`/cart?userEmail=${userEmail}`)
+  console.log('🚀 ~ getCarts ~ data:', data)
   return data
 }
 
-export async function getFavoriteCarts(): Promise<Cart[]> {
-  const { data } = await api.get<Cart[]>('/cart')
+export async function getFavoriteCarts(userEmail?: string): Promise<Cart[]> {
+  const { data } = await api.get<Cart[]>(`/cart?userEmail=${userEmail}`)
   return data.filter((cart) => cart.isFavorite)
 }
 
-export function useCarts(): UseQueryResult<Cart[], unknown> {
-  return useQuery(['carts'], () => getCarts(), {
-    staleTime: 1000 * 60 * 5 // 5 minutes
+export function useCarts(userEmail?: string): UseQueryResult<Cart[], unknown> {
+  return useQuery(['carts', userEmail], () => getCarts(userEmail), {
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    enabled: !!userEmail
   })
+}
+
+export function useFavoritesCarts(
+  userEmail?: string
+): UseQueryResult<Cart[], unknown> {
+  return useQuery(
+    ['favoritesCarts', userEmail],
+    () => getFavoriteCarts(userEmail),
+    {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      enabled: !!userEmail
+    }
+  )
 }
