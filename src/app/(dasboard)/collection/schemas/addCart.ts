ade2fabe15135628a447b2230558f420554fe99b
Andrey Araujo <andreyaraujo.dev@gmail.com>
2024-02-20 23:33:08 -0300

    refactor: atualizando formulario de cadastro de carro

--- src/app/(dasboard)/collection/schemas/addCart.ts
+++ src/app/(dasboard)/collection/schemas/addCart.ts
@@ -5,7 +5,15 @@ export const cartDataFormSchema = z
     model: z
       .string({ required_error: 'O modelo do carro é obrigatório' })
       .min(1, { message: 'O modelo do carro é obrigatório' }),
-    value: z.number().optional(),
+    value: z
+      .string()
+      .transform((val) => {
+        if (val !== '' || val !== undefined) {
+          return parseFloat(val.replace('.', '').replace(',', '.'))
+        }
+        return val
+      })
+      .optional(),
     year: z.number().optional(),
     purchaseDate: z.coerce.date().optional(),
     imageUrl: z.string().optional(),
